155c90cd55767f0eceedc05a6aaa10dce43d2bf1
Vllaadd <vlladdiimiir@gmail.com>
2023-03-23 04:48:31 -0400

    employee card created

---
+++ src/components/Employee.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Employee extends Component{
+    render(){
+        return(
+            <li className='list-group-item'>
+                <div className='row'>
+                    <img src={this.props.employee.picture.thumbnain} alt="employee profile"></img>
+                    <p className='col-md-2'>{this.props.employee.name.first}</p>
+                    <p className='col-md-2'>{this.props.employee.name.last}</p>
+                    <p className='col-md-2'>{this.props.employee.location.state}</p>
+                    <p className='col-md-2'>{this.props.employee.email}</p>
+                </div>
+            </li>
+        )
+    }
+}
+
+export default Employee;
