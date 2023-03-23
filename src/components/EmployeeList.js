3e0fca2012fe0bce32832a2a5e2aeac594bd558a
Vllaadd <vlladdiimiir@gmail.com>
2023-03-23 04:41:55 -0400

    employeelist render

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,15 +1,40 @@
 import React, {Component} from 'react';
+import Employee from "./Employee";
 
 class EmployeeList extends Component{
     constructor(props){
         super(props);
     }
-}
+
 render(){
     let filterEmployee = this.props.employee.filter(
         (employee) => {
             return 
-            employee.name.first.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+            employee.name.first.indexOf(this.state.search) !== -1;
         }
     )
+    return(
+        <div className='text-center'>
+            <br></br>
+            <input type="text"value={this.state.search}/>Search by EmployeeAPI
+            <div className='col-md-12'>
+                <ul>
+                    <div className='row'>
+                        <p className='col-md-2 font'>Image</p>
+                        <p className='col-md-2 font'>First Name</p>
+                        <p className='col-md-2 font'>Last Name</p>
+                        <p className='col-md-2 font'>Location</p>
+                        <p className='col-md-2 font'>Email</p>
+                    </div>
+                    {filterEmployee.map((employee) =>{
+                        return 
+                        <Employee employee={employee} key={employee.id} />
+                    })}
+                </ul>
+            </div>
+        </div>
+    )
 };
+};
+
+export default EmployeeList;
