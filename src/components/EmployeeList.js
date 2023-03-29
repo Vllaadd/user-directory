7a9bb4e72c7ac2d91c4cd0a3d0a6f16155a95bb8
Vllaadd <vlladdiimiir@gmail.com>
2023-03-29 03:56:48 -0400

    trying a different approach

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -5,42 +5,10 @@ class EmployeeList extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            users: [],
             search: ''
         }
     }
-
-    render() {
-        let filterEmployee = this.props.employee.filter(
-            (employee) => {
-                return employee.name.first.indexOf(this.state.search) !== -1;
-            }
-        );
-        return (
-            <div className='text-center'>
-                <br></br>
-                <input type="text" value={this.state.search} />Search by EmployeeAPI
-                <div className='col-md-12'>
-                    <br></br>
-                    <table>
-                            <tr>
-                                <th className='col-md-2 font'>Image</th>
-                                <th className='col-md-2 font'>First Name</th>
-                                <th className='col-md-2 font'>Last Name</th>
-                                <th className='col-md-2 font'>Location</th>
-                                <th className='col-md-2 font'>Email</th>
-                            </tr>
-                            
-                               {filterEmployee.map((employee) => {
-                                    return  <td>
-                                    <Employee employee={employee} key={employee.id} />
-                                    </td> 
-                                })}
-                                
-                        </table>
-                </div>
-            </div>
-        )
-    };
-};
+ 
 
 export default EmployeeList;
