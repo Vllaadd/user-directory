15d28e31b0199f2970663cce44e04f68112a70d6
Vllaadd <vlladdiimiir@gmail.com>
2023-03-29 04:47:58 -0400

    rendering a table of users

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -5,14 +5,14 @@ import Employee from "./Employee";
 class EmployeeList extends Component {
     constructor(props) {
         super(props);
-//set initial state•••••••••••••••••••••••••••••••••••••••••••••
+//set initial state•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
         this.state = {
             users: [],
             search: ''
         }
     }
 
-// once the component mounts, send a get request•••••••••••••••••
+// once the component mounts, send a get request••••••••••••••••••••••••••••••••••••
     componentDidMount(){
         function usersList(){
             return axios.get('https://randomuser.me/api/?results=200&nat=us');
@@ -31,6 +31,37 @@ class EmployeeList extends Component {
             })
             .catch(err => console.log(err));
         }
+
+// filtering the list to show only first and last name••••••••••••••••••••••••••••••
+    filteredUsers(){
+        const search = this.state.filter(user =>{
+            return (
+                user.first.toLowerCase().includes(search) ||
+                user.last.toLowerCase().includes(search)
+            );
+        });
+}
+
+
+//rendering the table of users••••••••••••••••••••••••••••••••••••••••••••••••••••••
+        renderUsers = () => {
+            return this.filteredUsers()
+                .sort(this.sortUsers)
+                .map((user, index) => {
+                    return(
+                        <tr key={index}>
+                            <td>
+                                <img src={user.image} alt='user'></img>
+                            </td>
+                            <td>{user.first}</td>
+                            <td>{user.last}</td>
+                            <td>{user.email}</td>
+                        </tr>
+                    )
+                })
+        }
+
+
     }
  
 
