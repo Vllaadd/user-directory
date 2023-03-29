82e369732ad013a7fed6020ef5baec0a8bf06557
Vllaadd <vlladdiimiir@gmail.com>
2023-03-29 02:12:26 -0400

    npm start didnt work deleted node modules

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Employee from "./Employee";
-import tableEmployee from "./tableEmployee";
 
 class EmployeeList extends Component {
     constructor(props) {
