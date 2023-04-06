91416b4c294dc989c43da4ffdf1866df008bddad
Vllaadd <vlladdiimiir@gmail.com>
2023-04-06 02:24:45 -0400

    Employee rendered onto the DOM

--- src/components/EmployeeAPI.js
+++ src/components/EmployeeAPI.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import EmployeeList from './EmployeeList';
-import Employee from './Employee';
 
 class EmployeeAPI extends Component{
     state = {
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -8,77 +8,10 @@ class EmployeeList extends React.Component {
             search: ''
         }
     }
-//set initial state•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 
-
-// once the component mounts, send a get request••••••••••••••••••••••••••••••••••••
-    componentDidMount(){
-        function usersList(){
-            return axios.get('https://randomuser.me/api/?results=200&nat=us');
-        };
-        usersList()
-            .then(res => {
-                    const userArray = res.data.results.map(user => {
-                        return {
-                            first: user.name.first,
-                            last: user.name.last,
-                            email:user.email
-                        }
-                        
-                    });
-                    console.log(userArray);
-                    this.setState({users:userArray});
-                    
-            })
-            .catch(err => console.log(err));
+    render(){
+        return <Employee employee={employee} key={employee.id} />
         }
-
-// filtering the list to show only first and last name••••••••••••••••••••••••••••••
-    filteredUsers(){
-        const search = this.state.search.toLowerCase();
-        return this.state.filter(user => {
-            return (
-                user.first.toLowerCase().includes(search) ||
-                user.last.toLowerCase().includes(search)
-            );
-        });
-}
-
-
-//rendering the table of users••••••••••••••••••••••••••••••••••••••••••••••••••••••
-        renderUsers = () => {
-            return this.map((user, index) => {
-                    return(
-                        <tr key={index}>
-                            {/* <td>
-                                <img src={user.image} alt='user'></img>
-                            </td> */}
-                            <td>{user.first}</td>
-                            <td>{user.last}</td>
-                            <td>{user.email}</td>
-                        </tr>
-                    )
-                })
-        }
-
-        render(){
-            return(
-                <>
-                <input placeholder='Search' />
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Image</th>
-                            <th>First</th>
-                            <th>Last</th>
-                        </tr>
-                    </thead>
-                </table>
-                </>
-                
-            )
-        }
-
 
     }
  
